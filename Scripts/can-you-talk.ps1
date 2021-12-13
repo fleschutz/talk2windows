6610d21a3f2231d53f7933f2ad22edc1fde14d14
Markus Fleschutz <markus@fleschutz.de>
2021-12-13 21:00:50 +0100

    Add can-you-talk.ps1

---
+++ Scripts/can-you-talk.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Replies to "Can you talk?"
+.DESCRIPTION
+	This script replies to 'Can you talk?' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./can-you-talk
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/give-reply.ps1" "Sure, why not."
+exit 0 # success
