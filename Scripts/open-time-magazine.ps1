e7b02537aaae04aa0ae5750c13e744ee002e415a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-14 08:07:14 +0100

    Add open-time-magazine.ps1

---
+++ Scripts/open-time-magazine.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Time Magazin website
+.DESCRIPTION
+	This script launches the Web browser with the Time Magazin website.
+.EXAMPLE
+	PS> ./open-time-magazin
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://time.com"
+exit 0 # success
