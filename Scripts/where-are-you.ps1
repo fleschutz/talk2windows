7ad3a9bcebaac8642682fc648c56a72d72405722
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-14 15:59:03 +0100

    Add where-are-you.ps1

---
+++ Scripts/where-are-you.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Replies to "Where are you?"
+.DESCRIPTION
+	This script replies to 'Where are you?' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./where-are-you
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/give-reply.ps1" "I'm in the machine."
+exit 0 # success
