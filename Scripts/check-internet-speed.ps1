a91645b133482e53cdae7cd60a401f2a62aeda3b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-15 14:23:59 +0100

    Add check-internet-speed.ps1

---
+++ Scripts/check-internet-speed.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Checks the Internet Speed
+.DESCRIPTION
+	This script launches the Web browser with Cloudflare's Speed Test website.
+.EXAMPLE
+	PS> ./check-internet-speed
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://speed.cloudflare.com/"
+exit 0 # success
