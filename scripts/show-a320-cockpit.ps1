1215380271b85ab43f30f9490e9069f7a7715844
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-16 07:50:03 +0100

    Add show-a320-cockpit.ps1 and show-tower-manual.ps1

---
+++ scripts/show-a320-cockpit.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the A320 cockpit
+.DESCRIPTION
+	This script launches the Web browser with the A320DP.com website.
+.EXAMPLE
+	PS> ./show-a320-cockpit
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "http://www.a320dp.com/A320_DP/panels.html"
+exit 0 # success
---
+++ scripts/show-tower-manual.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the ATTower manual
+.DESCRIPTION
+	This script launches the Web browser with the ATTower manual.
+.EXAMPLE
+	PS> ./show-tower-manual
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://ufa365-my.sharepoint.com/personal/markus_fleschutz_atcsim_de/_layouts/15/onedrive.aspx?id=%2Fsites%2FProductManuals%2FShared%20Documents%2FATTower&listurl=https%3A%2F%2Fufa365%2Esharepoint%2Ecom%2Fsites%2FProductManuals%2FShared%20Documents&viewid=52ccd99a%2D45b8%2D4a68%2D852d%2D0ba5ac10ef63"
+exit 0 # success
