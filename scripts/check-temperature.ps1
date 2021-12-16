06cea3283c09556c6e8567f2f7b73d3530e273a3
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-16 11:23:00 +0100

    Add check-temperature.ps1

---
+++ scripts/check-temperature.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+	Checks the temperature
+.DESCRIPTION
+	Queries the current temperature and replies by text-to-speech (TTS).
+.PARAMETER location
+	Specifies the location to use (determined automatically per default)
+.EXAMPLE
+	PS> ./check-temperature
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+param([string]$location = "") # empty means determine automatically
+
+try {
+	$Weather = (Invoke-WebRequest http://wttr.in/${location}?format=j1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
+	$Temp = $Weather.current_condition.temp_C
+	
+	& "$PSScriptRoot/give-reply.ps1" "$($Temp)°C outside."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
