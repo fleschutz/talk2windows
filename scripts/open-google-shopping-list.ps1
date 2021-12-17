c044cfd3534e75ce092dccbf910ab8576920e687
Markus Fleschutz <markus@fleschutz.de>
2021-12-17 15:36:42 +0100

    Add open-shopping-list.ps1

---
+++ scripts/open-google-shopping-list.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens Google Shopping List
+.DESCRIPTION
+	This script launches the Web browser with the Google Shopping List website.
+.EXAMPLE
+	PS> ./open-google-shopping-list
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://shoppinglist.google.com"
+exit 0 # success
