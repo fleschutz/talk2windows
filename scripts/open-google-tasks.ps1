a91bd9315ce12562be2e506e963c2e5a213d6bb7
Markus Fleschutz <markus@fleschutz.de>
2021-12-17 15:48:32 +0100

    Add open-google-tasks.ps1

---
+++ scripts/open-google-tasks.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens Google Tasks
+.DESCRIPTION
+	This script launches the Web browser with the Google Tasks website.
+.EXAMPLE
+	PS> ./open-google-tasks
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://tasks.google.com/embed/?origin=https://calendar.google.com&fullWidth=1"
+exit 0 # success
