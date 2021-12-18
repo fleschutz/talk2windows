7112c13bca97c8741e004a6f7b1e3887c0cea3e0
Markus Fleschutz <markus@fleschutz.de>
2021-12-18 14:11:38 +0100

    Fix typo

--- scripts/_set-wallpaper.ps1
+++ scripts/_set-wallpaper.ps1
@@ -63,8 +63,6 @@ try {
 	if ($ImageFile -eq "" ) { $ImageFile = read-host "Enter path to image file" }
 
 	SetWallPaper -Image $ImageFile -Style $Style
-
-	"✔️  Done."
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
