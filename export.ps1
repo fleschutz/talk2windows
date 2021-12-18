efc196d39a3a83a26aa6b3d49b2bfd2a1172495a
Markus Fleschutz <markus@fleschutz.de>
2021-12-18 14:09:20 +0100

    Update export.ps1

--- export.ps1
+++ export.ps1
@@ -23,21 +23,30 @@
 
 param([string]$WakeWord = "Windows", [string]$FilePattern = "$PSScriptRoot/scripts/*.ps1", [string]$Application = "terminal", [string]$TargetFile = "$HOME\.serenade\scripts\talk2windows.js")
 
+function GetLine1 { param([string]$WakeWord, [string]$Keywords, [string]$ScriptName)
+	return "serenade.global().command(`"$WakeWord $Keywords`",async(api)=>{await api.focusApplication(`"$Application`");await api.pressKey(`"return`");await api.typeText(`"$ScriptName.ps1`");await api.pressKey(`"return`");});" 
+}
+
+function GetLine2 { param([string]$WakeWord, [string]$Keywords, [string]$ScriptPath)
+	$ScriptPath = $ScriptPath -replace "\\","\\"
+	return "serenade.global().command(`"$WakeWord $Keywords`",async(api)=>{await api.runShell(`"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`", [ `"$ScriptPath`" ]);});"
+}
+
 try {
 	"(1) Wake word is: `'$WakeWord`'"
+	$WakeWord = $WakeWord.toLower()
 
 	$Scripts = Get-ChildItem "$FilePattern"
 	"(2) Found $($Scripts.Count) PowerShell scripts in subfolder `'scripts`'"
 
 	"(3) Writing custom JavaScript file: $TargetFile..."
 	"/* DO NOT EDIT! This file has been generated automatically by talk2windows */" | Set-Content "$TargetFile"
-	foreach ($Script in $Scripts) {
+	foreach($Script in $Scripts) {
 		$ScriptName = $Script.basename
 		if ($ScriptName[0] -eq "_") { continue } # internal script, don't export it
-		$Keyword = $ScriptName -replace "-"," "
-		"serenade.global().command(`"$($WakeWord.toLower()) $Keyword`",async(api)=>{await api.focusApplication(`"$Application`");await api.pressKey(`"return`");await api.typeText(`"$ScriptName.ps1`");await api.pressKey(`"return`");});" | Add-Content "$TargetFile"
+		$Keywords = $ScriptName -replace "-"," "
+		GetLine2 $WakeWord $Keywords $Script | Add-Content "$TargetFile"
 	}
-
 	"Export to Serenade was successful - launch Serenade now to talk to Windows."
 	exit 0 # success
 } catch {
