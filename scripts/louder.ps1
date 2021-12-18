d6ff91ba5afeddb7b003ddfefd203dd7b7459099
Markus Fleschutz <markus@fleschutz.de>
2021-12-18 15:37:39 +0100

    Add louder.ps1

---
+++ scripts/louder.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+	Turns the volume up
+.DESCRIPTION
+	This PowerShell script turns the audio volume up (by +10% by default)
+.PARAMETER percent
+	Specifies the percent number
+.EXAMPLE
+	PS> ./louder
+.NOTES
+	Author:  Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+param([int]$percent = 10)
+
+try {
+	$obj = New-Object -com wscript.shell
+	for ([int]$i = 0; $i -lt $percent; $i += 2) {
+		$obj.SendKeys([char]175) # each tick is +2%
+	}
+	& "$PSScriptRoot/give-reply.ps1" "OK, $($percent)% louder."
+	exit 0 # success
+} catch {
+	"Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
