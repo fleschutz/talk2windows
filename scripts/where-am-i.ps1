9a12dfc53da06029bbd383c40ac15a3192731b53
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-21 07:47:46 +0100

    Update where-am-i.ps1

--- scripts/where-am-i.ps1
+++ scripts/where-am-i.ps1
@@ -15,8 +15,9 @@ try {
 	$Weather = (Invoke-WebRequest http://wttr.in/?format=j1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	$Area = $Weather.nearest_area.areaName.value
 	$Region = $Weather.nearest_area.region.value
+	$Country = $Weather.nearest_area.country.value
 
-	& "$PSScriptRoot/_reply.ps1" "Near $Area, $Region."
+	& "$PSScriptRoot/_reply.ps1" "You're near $Area in $Region ($Country)."
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])."
