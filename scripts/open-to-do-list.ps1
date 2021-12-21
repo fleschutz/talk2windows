3d4297387b8f911f1ac9afc1fea15671386001c3
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-21 09:51:43 +0100

    Add open-to-do-list.ps1

---
+++ scripts/open-to-do-list.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Opens Google Tasks
+.DESCRIPTION
+	This PowerShell script launches the Web browser with the Google Tasks website.
+.EXAMPLE
+	PS> ./open-to-do-list
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://tasks.google.com/embed/?origin=https://calendar.google.com&fullWidth=1"
+exit 0 # success
