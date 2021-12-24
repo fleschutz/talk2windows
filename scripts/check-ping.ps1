cdcbd3345207c34380632a791e8802386e798ede
Markus Fleschutz <markus@fleschutz.de>
2021-12-24 09:51:28 +0100

    Update check-ping.ps1

--- scripts/check-ping.ps1
+++ scripts/check-ping.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-	Checks the ping latency from the local computer to selected Internet hosts
+	Checks the Ping Time
 .DESCRIPTION
 	This PowerShell script checks the ping latency from the local computer to selected Internet hosts.
 	The hosts by default are: amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com
@@ -9,7 +9,7 @@
 .EXAMPLE
 	PS> ./check-ping
 .NOTES
-	Author: Markus Fleschutz · License: CC0
+	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
 #>
@@ -17,7 +17,8 @@
 param([string]$hosts = "amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com")
 
 try {
-	write-progress "Sending pings to $hosts..."
+	& "$PSScriptRoot/_reply.ps1" "Sending a ping to 9 internet hosts..."
+
 	$HostsArray = $hosts.Split(",")
 	$Pings = test-connection -count 1 -computerName $HostsArray
 
@@ -36,9 +37,9 @@ try {
 	}
 	$Avg = $Avg / $Pings.count
 
-	& "$PSScriptRoot/_reply.ps1" "$($Avg)ms average ping time ($($Min)ms min, $($Max)ms max)"
+	& "$PSScriptRoot/_reply.ps1" "Ping is $($Avg)ms average, ($($Min)ms min, $($Max)ms max."
 	exit 0 # success
 } catch {
-	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
 	exit 1
 }
