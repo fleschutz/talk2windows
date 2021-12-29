9a56e4114f764793f388a062320951b3cd49824f
Markus Fleschutz <markus@fleschutz.de>
2021-12-29 12:13:46 +0100

    Add play-iss-docking-simulator-game.ps1

---
+++ scripts/play-iss-docking-simulator-game.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Plays the ISS Docking Simulator game
+.DESCRIPTION
+	This PowerShell script launches the Web browser with the ISS Docking Simulator game.
+.EXAMPLE
+	PS> ./play-iss-docking-simulator-game
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://iss-sim.spacex.com"
+exit 0 # success
