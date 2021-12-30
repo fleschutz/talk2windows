d06a225a2d0b485f7902aeda72c01d133859702f
Markus <markus@fleschutz.de>
2021-12-30 19:24:38 +0100

    Simple rename

--- scripts/spell-word-XYZ.ps1
+++ scripts/spell-word-XYZ.ps1
@@ -16,8 +16,6 @@
 param([string]$word = "")
 
 try {
-	if ($word -eq "" ) { $word = read-host "Enter word to spell" }
-
 	[char[]]$ArrayOfChars = $word.ToUpper()
 	$Reply = ""
 	foreach($Char in $ArrayOfChars) {
@@ -27,6 +25,6 @@ try {
 	& "$PSScriptRoot/_reply.ps1" "$Reply"
 	exit 0 # success
 } catch {
-        "⚠️Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+        & "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
         exit 1
 }
