b90c325ab50d595cbef9590f3405bd5bac1c5373
Markus Fleschutz <markus@fleschutz.de>
2022-01-10 11:46:24 +0100

    Rename to empty-recycle-bin.ps1

--- scripts/empty-recycle-bin.ps1
+++ scripts/empty-recycle-bin.ps1
@@ -1,10 +1,10 @@
 <#
 .SYNOPSIS
-	Clears the recycle bin
+	Empties the Recycle Bin
 .DESCRIPTION
-	This PowerShell script removes the content of the recycle bin folder permanently. NOTE: this cannot be undo!
+	This PowerShell script removes the content of all the local computer's recycle bins permanently. NOTE: this cannot be undo!
 .EXAMPLE
-	PS> ./clear-recycle-bin
+	PS> ./empty-recycle-bin
 .NOTES
 	Author: Markus Fleschutz / License: CC0
 .LINK
@@ -15,7 +15,7 @@ try {
 	Clear-RecycleBin -Force -Confirm:$false
 #	if ($lastExitCode -ne "0") { throw "'Clear-RecycleBin' failed" }
 
-	& "$PSScriptRoot/_reply.ps1" "Clean now."
+	& "$PSScriptRoot/_reply.ps1" "Done."
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
