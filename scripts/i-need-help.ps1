5121f0525d26ab16b86b1c700e83abe19e0eb8b2
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-01 15:21:54 +0100

    Update i-need-help.ps1

--- scripts/i-need-help.ps1
+++ scripts/i-need-help.ps1
@@ -1,15 +1,18 @@
 <#
 .SYNOPSIS
-	Shows the voice manual
+	Shows the Manual for Talk2Windows
 .DESCRIPTION
-	This PowerShell script launches the Web browser with the voice control manual.
+	This PowerShell script launches the Web browser with the manual for Talk2Windows.
 .EXAMPLE
 	PS> ./i-need-help
 .NOTES
 	Author: Markus Fleschutz / License: CC0
 .LINK
-	https://github.com/fleschutz/PowerShell
+	https://github.com/fleschutz/talk2windows
 #>
+
+$Reply = "Okay.", "Hold on.", "Just a second.", "Help is on the way." | Get-Random
+& "$PSScriptRoot/_reply.ps1" $Reply
 
 & "$PSScriptRoot/open-browser.ps1" "https://github.com/fleschutz/talk2windows"
 exit 0 # success
