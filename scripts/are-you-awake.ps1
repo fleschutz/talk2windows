3ba8f26ed64227c4fd8492fee38e58cecca4426b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-03 07:37:51 +0100

    Add are-you-awake.ps1

---
+++ scripts/are-you-awake.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+	Replies to "Are you awake?"
+.DESCRIPTION
+	This PowerShell script replies to 'Are you awake?' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./are-you-awake
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+$Reply = "Yes.", "Sure.", "Yes, sure.", "Always." | Get-Random
+& "$PSScriptRoot/_reply.ps1" "$Reply"
+exit 0 # success
