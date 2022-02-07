bbb30241b61a4d4ca7f4659befae7e1c6e8ab479
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-07 11:07:13 +0100

    Add open-sunny-portal.ps1

---
+++ scripts/open-sunny-portal.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Opens the Sunny Portal
+.DESCRIPTION
+	This PowerShell script launches the Web browser with the SMA Sunny Portal website.
+.EXAMPLE
+	PS> ./open-sunny-portal
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://sunnyportal.com"
+exit 0 # success
