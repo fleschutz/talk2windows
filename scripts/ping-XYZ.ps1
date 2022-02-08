7c33a6f1dbb508c58cebddc07ca50022e18b29d0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-08 12:10:56 +0100

    Update ping-XYZ.ps1

--- scripts/ping-XYZ.ps1
+++ scripts/ping-XYZ.ps1
@@ -1,33 +1,33 @@
 <#
 .SYNOPSIS
-	Ping a Host
+	Ping a host
 .DESCRIPTION
 	This PowerShell script checks the ping latency from the local computer to the given host.
-.PARAMETER Hostname
-	Specifies the hostname or IP address
 .EXAMPLE
-	PS> ./ping-XYZ myhost
+	PS> ./ping-XYZ host123
 .NOTES
 	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
 #>
 
-param([string]$Part1 = "", [string]$Part2 = "", [string]$Part3 = "")
+param([string]$Part1 = "", [string]$Part2 = "", [string]$Part3 = "", [string]$Part4 = "")
 
 try {
 	& "$PSScriptRoot/_reply.ps1" "OK."
-	$Hostname = "$($Part1)$($Part2)$($Part3)"
+	$Hostname = "$Part1$Part2$Part3$Part4"
+	$Hostname = $Hostname -replace " ",""
 
 	$Pings = Test-Connection -count 1 -computerName $Hostname
         if ($Pings.count -eq 0) {
-                & "$PSScriptRoot/_reply.ps1" "Sorry, '$Hostname' is offline."
+                $Reply = "Sorry, '$Hostname' is offline."
         } else {
                 foreach($Ping in $Pings) {
                         if ($IsLinux) { $Latency = $Ping.latency } else { $Latency = $Ping.ResponseTime }
                 }
-                & "$PSScriptRoot/_reply.ps1" "'$Hostname' is online with $Latency ms latency."
+                $Reply = "'$Hostname' is online with $Latency ms latency."
         }
+	& "$PSScriptRoot/_reply.ps1" $Reply
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
