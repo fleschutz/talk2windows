6c20929521756b79007b3925339b9d1d41783c8a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-15 11:48:25 +0100

    Add show-emojis.ps1

---
+++ scripts/show-emojis.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+	Shows the Emoji Keyboard
+.DESCRIPTION
+	This PowerShell script show the emoji keyboard to insert characters
+.EXAMPLE
+	PS> ./show-emojis
+.NOTES
+	Author:  Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("^{ESC}")
+	& "$PSScriptRoot/_reply.ps1" "OK."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
