20541faf14b7640d90f9953ac739c2f5bcbc3207
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-15 09:02:36 +0100

    Update thanks.ps1

--- scripts/thanks.ps1
+++ scripts/thanks.ps1
@@ -1,17 +1,16 @@
 <#
 .SYNOPSIS
-	Replies to 'thanks'
+	Replies to 'Thanks'
 .DESCRIPTION
-	This PowerShell script replies to 'thanks' by text-to-speech (TTS).
+	This PowerShell script replies to 'Thanks' by text-to-speech (TTS).
 .EXAMPLE
 	PS> ./thanks
 .NOTES
-	Author: Markus Fleschutz · License: CC0
+	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
 #>
 
-$Reply = "You're welcome.", "You're very welcome.", "That's all right.", "No problem.", "No worries.", "Don't mention it.", "It's my pleasure.", "My pleasure.", "Pleasure is mine.", "Glad to help.", "Anytime." | Get-Random
-
-& "$PSScriptRoot/_reply.ps1" "$Reply"
+$Reply = "You're welcome.", "No worries.", "Don't mention it.", "My pleasure.", "Pleasure is mine.", "Glad to help.", "Anytime." | Get-Random
+& "$PSScriptRoot/_reply.ps1" $Reply
 exit 0 # success
