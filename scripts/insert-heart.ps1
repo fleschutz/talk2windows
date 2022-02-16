514333024907c242a6304e5fd6df4aad07162aa8
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-16 11:31:08 +0100

    Add insert-heart.ps1

---
+++ scripts/insert-heart.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Inserts a Heart
+.DESCRIPTION
+	This PowerShell script inserts a heart Unicode character.
+.EXAMPLE
+	PS> ./insert-heart
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	Set-Clipboard -value "$([char]0x2665)"
+
+    $obj = New-Object -com wscript.shell
+    $obj.SendKeys("^V")
+    & "$PSScriptRoot/_reply.ps1" "Okay."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
