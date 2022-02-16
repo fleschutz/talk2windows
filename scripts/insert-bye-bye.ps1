4ac8d7c6bbdb58d2975e4a1bede96155d4d98b2b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-16 14:48:42 +0100

    Add insert-bye-bye.ps1

---
+++ scripts/insert-bye-bye.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Inserts a Bye Bye
+.DESCRIPTION
+	This PowerShell script inserts "bye bye".
+.EXAMPLE
+	PS> ./insert-bye-bye
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("bye bye")
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
