4d6b840be63a1f034638fb77db9dbab81c9ed751
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-16 14:27:27 +0100

    Add insert-smiley.ps1

---
+++ scripts/insert-smiley.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+	Inserts a Smiley
+.DESCRIPTION
+	This PowerShell script inserts a smiley Unicode character.
+.EXAMPLE
+	PS> ./insert-smiley
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+	
+	Set-Clipboard -value "😊"
+
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("^V")
+	& "$PSScriptRoot/_reply.ps1" "Done."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
