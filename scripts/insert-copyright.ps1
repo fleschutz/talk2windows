021cca3de1b0b024a3239eee47c6035d6d849ade
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-16 15:19:53 +0100

    Add insert-clapping.ps1 and insert-copyright.ps1

---
+++ scripts/insert-copyright.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Inserts a Copyright Symbol
+.DESCRIPTION
+	This PowerShell script inserts the copyright symbol.
+.EXAMPLE
+	PS> ./insert-copyright
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("©")
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
