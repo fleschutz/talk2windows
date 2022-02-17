f0e98c14e84adc94a3ca635895e7abd04b483690
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-17 07:08:51 +0100

    Update open-toggle-track.ps1

--- scripts/open-toggle-track.ps1
+++ scripts/open-toggle-track.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Opens Toggl Track
 .DESCRIPTION
-	This PowerShell script launches the Web browser with Toggl Track.
+	This PowerShell script launches the Web browser with the Toggl Track website.
 .EXAMPLE
 	PS> ./open-toggle-track
 .NOTES
@@ -11,5 +11,6 @@
 	https://github.com/fleschutz/talk2windows
 #>
 
+& "$PSScriptRoot/_reply.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "https://track.toggl.com"
 exit 0 # success
