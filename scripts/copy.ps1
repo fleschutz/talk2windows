b66f5ccbd090b3f29288ec5b855774355d1cc7fd
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-17 13:23:33 +0100

    Add copy.ps1 and paste.ps1

---
+++ scripts/copy.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Copy the Selection
+.DESCRIPTION
+	This PowerShell script copies the selection.
+.EXAMPLE
+	PS> ./copy
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("^C")
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
---
+++ scripts/paste.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Pastes the Selection
+.DESCRIPTION
+	This PowerShell script paste the selection.
+.EXAMPLE
+	PS> ./paste
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("^V")
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
