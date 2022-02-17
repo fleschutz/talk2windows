570de108517bfd5c77ea6e158c55e79c4b661fb0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-17 07:32:30 +0100

    Add insert-okay.ps1 and insert-trademark.ps1

---
+++ scripts/insert-okay.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Inserts OK
+.DESCRIPTION
+	This PowerShell script inserts "OK".
+.EXAMPLE
+	PS> ./insert-okay
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("OK")
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
--- scripts/open-distro-watch-website.ps1
+++ scripts/open-distro-watch-website.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-	Opens the DistroWatch website
+	Opens DistroWatch
 .DESCRIPTION
 	This PowerShell script launches the Web browser with the DistroWatch website.
 .EXAMPLE
@@ -11,5 +11,6 @@
 	https://github.com/fleschutz/talk2windows
 #>
 
+& "$PSScriptRoot/_reply.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "https://distrowatch.com/"
 exit 0 # success
