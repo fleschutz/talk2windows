80f785af50a95d27bc7ca42806ef5195e2d3c99f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-18 13:18:56 +0100

    Add insert-date.ps1

---
+++ scripts/insert-date.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Insert the Date
+.DESCRIPTION
+	This PowerShell script inserts the current date.
+.EXAMPLE
+	PS> ./insert-date
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+
+        [system.threading.thread]::currentthread.currentculture = [system.globalization.cultureinfo]"en-US"
+        $Weekday = (Get-Date -format "dddd")
+        $CurrentDate = (Get-Date).ToShortDateString()
+        $Today = "$Weekday, $CurrentDate"
+
+	$obj = New-Object -com wscript.shell
+	$obj.SendKeys("$Today")
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
