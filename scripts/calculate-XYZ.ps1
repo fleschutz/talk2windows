0df9286732e9db4f61d04a4985b10c71f0a6ebf6
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-24 10:51:11 +0100

    Update calculate-XYZ.ps1

--- scripts/calculate-XYZ.ps1
+++ scripts/calculate-XYZ.ps1
@@ -11,10 +11,10 @@
 	https://github.com/fleschutz/talk2windows
 #>
 
-param([string]$Part1 = "", [string]$Part2 = "", [string]$Part3 = "", [string]$Part4 = "", [string]$Part5 = "", [string]$Part6 = "")
+param([string]$Part1 = "", [string]$Part2 = "", [string]$Part3 = "", [string]$Part4 = "", [string]$Part5 = "", [string]$Part6 = "", [string]$Part7 = "")
 
 try {
-	$Expr = "$Part1 $Part2 $Part3 $Part4 $Part5 $Part6"
+	$Expr = "$Part1 $Part2 $Part3 $Part4 $Part5 $Part6 $Part7"
 	$Expr = $Expr -replace "one","1"
 	$Expr = $Expr -replace "two","2"
 	$Expr = $Expr -replace "three","3"
@@ -25,18 +25,20 @@ try {
 	$Expr = $Expr -replace "eight","8"
 	$Expr = $Expr -replace "nine","9"
 	$Expr = $Expr -replace "ten","10"
+	$Expr = $Expr -replace " and a half",".5"
+	$Expr = $Expr -replace " and a quarter",".25"
 	$Expr = $Expr -replace "point","."
 	$Expr = $Expr -replace "plus","+"
 	$Expr = $Expr -replace "minus","-"
-	$Expr = $Expr -replace "multiplied","*"
-	$Expr = $Expr -replace "divided","/"
+	$Expr = $Expr -replace "multiplied by","*"
+	$Expr = $Expr -replace "divided by","/"
 	$Expr = $Expr -replace "by",""
 	$Expr = $Expr -replace "e","2.71828182845905"
 	$Expr = $Expr -replace "pi","3.14159265358979"
 	$Expr = $Expr -replace "tau","6.283185307179586"
 	$Expr = $Expr -replace "gravity","9.81"
 	$Result = Invoke-Expression $Expr
-	& "$PSScriptRoot/_reply.ps1" "It's $Result."
+	& "$PSScriptRoot/_reply.ps1" "It's $Result"
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
