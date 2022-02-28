09cecd93322be0e9b14c2ea2a4379391eb95d94d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-02-28 16:16:55 +0100

    Add insert-good-bye.ps1

---
+++ scripts/insert-good-bye.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Inserts a Good-bye
+.DESCRIPTION
+	This PowerShell script inserts 'Good-bye' at the current text cursor position.
+.EXAMPLE
+	PS> ./insert-good-bye
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/talk2windows
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Okay."
+	$ws = New-Object -com wscript.shell
+	$ws.SendKeys("Good-bye")
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
