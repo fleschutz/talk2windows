7116d1b70c6b16c961dc8d00c66a454cd1e99081
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-14 08:34:12 +0100

    Update voice response

--- scripts/open-focus-magazine.ps1
+++ scripts/open-focus-magazine.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Opens the Focus Magazine
 .DESCRIPTION
-	This PowerShell script launches the Web browser with the Focus magazine website.
+	This PowerShell script launches the Web browser with the German Focus magazine website.
 .EXAMPLE
 	PS> ./open-focus-magazine
 .NOTES
@@ -11,5 +11,6 @@
 	https://github.com/fleschutz/talk2windows
 #>
 
+& "$PSScriptRoot/_reply.ps1" "Focus..."
 & "$PSScriptRoot/open-browser.ps1" "https://www.focus.de"
 exit 0 # success
