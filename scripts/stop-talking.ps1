a6e7c8a828b272fdb4b02cd84b6dfce522ad7726
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-30 07:42:03 +0200

    Add stop-talking.ps1

---
+++ scripts/stop-talking.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+	Turns the volume off
+.DESCRIPTION
+	This PowerShell script mutes the audio device immediately.
+.EXAMPLE
+	PS> ./stop-talking.ps1
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	$obj = new-object -com wscript.shell
+	$obj.SendKeys([char]173)
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
