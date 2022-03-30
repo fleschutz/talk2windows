8dfd77c9a0ac495195a5cb711996cf380d91f41e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-30 13:09:36 +0200

    Update play-pop-music.ps1 and play-rock-music.ps1

--- scripts/play-pop-music.ps1
+++ scripts/play-pop-music.ps1
@@ -5,11 +5,12 @@
 	This PowerShell script launches the Web browser and plays pop music.
 .EXAMPLE
 	PS> ./play-pop-music
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
+& "$PSScriptRoot/_reply.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "http://streema.com/radios/play/Nick_Radio"
 exit 0 # success
--- scripts/play-rock-music.ps1
+++ scripts/play-rock-music.ps1
@@ -5,11 +5,12 @@
 	This PowerShell script launches the Web browser and plays rock music.
 .EXAMPLE
 	PS> ./play-rock-music
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
+& "$PSScriptRoot/_reply.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "http://streema.com/radios/play/RADIO_BOB_BOBs_Alternative"
 exit 0 # success
