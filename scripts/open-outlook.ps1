b49c268554269496032d85bc9fd94779adfe886b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-30 13:13:19 +0200

    Update open-outlook.ps1

--- scripts/open-outlook.ps1
+++ scripts/open-outlook.ps1
@@ -4,11 +4,11 @@
 .DESCRIPTION
 	This PowerShell script launches the Microsoft Outlook email application.
 .EXAMPLE
-	PS> ./open-outlook
-.NOTES
-	Author: Markus Fleschutz / License: CC0
+	PS> ./open-outlook.ps1
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 function TryToExec { param($Folder, $Binary)
@@ -18,10 +18,10 @@ function TryToExec { param($Folder, $Binary)
 }
 
 try {
-	& "$PSScriptRoot/_reply.ps1" "Outlook..."
+	& "$PSScriptRoot/_reply.ps1" "Okay."
 	TryToExec "C:\Program Files\Microsoft Office\root\Office16" "OUTLOOK.EXE"
 	TryToExec "C:\Programs\Microsoft Office\Office14" "OUTLOOK.EXE"
-	throw "Outlook isn't installed."
+	throw "Outlook isn't installed yet."
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
 	exit 1
