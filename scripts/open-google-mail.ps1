c5cd5dff81d82c26d7d5343897519ef062ea1f01
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-04 12:12:15 +0200

    Update open-google-mail.ps1

--- scripts/open-google-mail.ps1
+++ scripts/open-google-mail.ps1
@@ -5,12 +5,12 @@
 	This PowerShell script launches the Web browser with Google Mail.
 .EXAMPLE
 	PS> ./open-google-mail
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-& "$PSScriptRoot/_reply.ps1" "Mail..."
+& "$PSScriptRoot/_reply.ps1" "Hold on."
 & "$PSScriptRoot/open-browser.ps1" "https://mail.google.com"
 exit 0 # success
