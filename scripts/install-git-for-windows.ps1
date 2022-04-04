43ba01c8138055321daab5222ecef67eae32aa50
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-04 14:32:11 +0200

    Add install-git-for-windows.ps1

---
+++ scripts/install-git-for-windows.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+	Installs Git for Windows
+.DESCRIPTION
+	This PowerShell script installs Git for Windows.
+.EXAMPLE
+	PS> ./install-git-for-windows
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Installing Git for Windows, please wait..."
+
+	& winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements
+	if ($lastExitCode -ne "0") { throw "'winget install' failed" }
+
+	& "$PSScriptRoot/_reply.ps1" "Git for Windows is installed now."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
