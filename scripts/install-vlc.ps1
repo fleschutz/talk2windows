437250288f21ef053310a4e58f68feb914384a9b
Markus Fleschutz <markus@fleschutz.de>
2022-04-05 16:36:02 +0200

    Update install-vlc.ps1

--- scripts/install-vlc.ps1
+++ scripts/install-vlc.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Installs VLC
 .DESCRIPTION
-	This PowerShell script installs the VLC media player from the Microsoft Store.
+	This PowerShell script installs the VLC media player.
 .EXAMPLE
 	PS> ./install-vlc
 .LINK
@@ -14,7 +14,7 @@
 try {
 	& "$PSScriptRoot/_reply.ps1" "Installing VLC media player, please wait..."
 
-	& winget install "VLC" --source msstore --accept-package-agreements --accept-source-agreements
+	& winget install --id VideoLAN.VLC --accept-package-agreements --accept-source-agreements
 	if ($lastExitCode -ne "0") { throw "'winget install' failed" }
 
 	& "$PSScriptRoot/_reply.ps1" "VLC media player installed successfully."
