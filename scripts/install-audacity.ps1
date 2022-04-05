e590123daa0add360f5fc5842421ff88481ba9bb
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-05 16:28:47 +0200

    Add install-audacity.ps1 and install-vivaldi.ps1

---
+++ scripts/install-audacity.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+	Installs Audacity
+.DESCRIPTION
+	This PowerShell script installs Audacity.
+.EXAMPLE
+	PS> ./install-audacity
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Installing Audacity, please wait..."
+
+	& winget install --id Audacity.Audacity --accept-package-agreements --accept-source-agreements
+	if ($lastExitCode -ne "0") { throw "'winget install' failed" }
+
+	& "$PSScriptRoot/_reply.ps1" "Audacity installed successfully."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
