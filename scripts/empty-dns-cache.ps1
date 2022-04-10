9d6b2274e62773ce929cb4072c4cd148d6580125
Markus <markus@fleschutz.de>
2022-04-10 11:21:02 +0200

    Update empty-dns-cache.ps1

--- scripts/empty-dns-cache.ps1
+++ scripts/empty-dns-cache.ps1
@@ -2,18 +2,18 @@
 .SYNOPSIS
 	Empty the DNS cache
 .DESCRIPTION
-	This PowerShell script empties the DNS (domain name system) cache.
+	This PowerShell script empties the DNS (domain name system) client cache.
 .EXAMPLE
 	PS> ./empty-dns-cache
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 try {
-	& "$PSScriptRoot/_reply.ps1" "Okay."
-	& ipconfig /flushdns
+	Clear-DnsClientCache
+	& "$PSScriptRoot/_reply.ps1" "DNS cache is empty now."	
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
