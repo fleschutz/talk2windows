8c20fbaa86a8df34794501303989de29d0040184
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-12 11:59:13 +0200

    Update check-temperature.ps1

--- scripts/check-temperature.ps1
+++ scripts/check-temperature.ps1
@@ -16,12 +16,18 @@ try {
 	$Weather = (Invoke-WebRequest http://wttr.in/${Location}?format=j1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	[int]$CurTemp = $Weather.current_condition.temp_C
 	[int]$MaxTemp = -100
+	[int]$MaxTime = 0
 	foreach($Hourly in $Weather.weather.hourly) {
 		[int]$Temp = $Hourly.tempC
-		if ($Temp -gt $MaxTemp) { $MaxTemp = $Temp }
+		if ($Temp -gt $MaxTemp) { $MaxTemp = $Temp; $MaxTime = $Hourly.time }
 		if ($Hourly.time -eq "2100") { break }
 	}
-	if ($CurTemp -eq $MaxTemp) { $Reply = "$CurTemp °C outside." } else { $Reply = "$CurTemp °C outside, up to $MaxTemp °C expected today." }
+	if ($CurTemp -eq $MaxTemp) {
+		$Reply = "$CurTemp °C outside."
+	} else {
+		if ($MaxTime -lt 1200) { $Time = "$($MaxTime / 100) AM" } else { $Time = "$(($MaxTime - 1200) / 100) PM" }
+		$Reply = "$CurTemp °C outside, up to $MaxTemp °C expected at $Time."
+	}
 	& "$PSScriptRoot/_reply.ps1" $Reply
 	exit 0 # success
 } catch {
