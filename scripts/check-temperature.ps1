2ac426df09b0f83ab24604fbc0ba8157ac0930d3
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-12 11:15:33 +0200

    Update check-temperature.ps1

--- scripts/check-temperature.ps1
+++ scripts/check-temperature.ps1
@@ -3,30 +3,26 @@
 	Checks the Temperature
 .DESCRIPTION
 	Queries the current temperature and replies by text-to-speech (TTS).
-.PARAMETER location
-	Specifies the location to use (determined automatically per default)
 .EXAMPLE
 	PS> ./check-temperature
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-param([string]$location = "") # empty means determine automatically
-
 try {
-	$Weather = (Invoke-WebRequest http://wttr.in/${location}?format=j1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
+	$Location = "" # empty means determine automatically
+	$Weather = (Invoke-WebRequest http://wttr.in/${Location}?format=j1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	[int]$CurTemp = $Weather.current_condition.temp_C
-	[int]$MinTemp = 100
 	[int]$MaxTemp = -100
-	foreach ($Hourly in $Weather.weather.hourly) {
+	foreach($Hourly in $Weather.weather.hourly) {
 		[int]$Temp = $Hourly.tempC
-		if ($Temp -lt $MinTemp) { $MinTemp = $Temp }
 		if ($Temp -gt $MaxTemp) { $MaxTemp = $Temp }
 		if ($Hourly.time -eq "2100") { break }
 	}
-	& "$PSScriptRoot/_reply.ps1" "$CurTemp °C outside, ranging from $MinTemp to $MaxTemp °C today."
+	if ($CurTemp -eq $MaxTemp) { $Reply = "$CurTemp °C outside." } else { $Reply = "$CurTemp °C outside, up to $MaxTemp °C expected today." }
+	& "$PSScriptRoot/_reply.ps1" $Reply
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
