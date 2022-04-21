1becb7c1a317132eceb721caf48b558aaf362fa0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-21 08:12:00 +0200

    Update check-weather.ps1

--- scripts/check-weather.ps1
+++ scripts/check-weather.ps1
@@ -2,15 +2,13 @@
 .SYNOPSIS
 	Checks the weather 
 .DESCRIPTION
-	Checks the current weather report.
-.PARAMETER location
-	Specifies the location to use (determined automatically per default)
+	Queries the current weather report and tells it by text-to-speech (TTS).
 .EXAMPLE
 	PS> ./check-weather
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 param([string]$location = "") # empty means determine automatically
@@ -21,17 +19,12 @@ try {
 	$Temp = $Weather.current_condition.temp_C
 	$Precip = $Weather.current_condition.precipMM
 	$Humidity = $Weather.current_condition.humidity
-	$Pressure = $Weather.current_condition.pressure
 	$WindSpeed = $Weather.current_condition.windspeedKmph
 	$WindDir = $Weather.current_condition.winddir16Point
-	$UV = $Weather.current_condition.uvIndex
-	$Visib = $Weather.current_condition.visibility
 	$Clouds = $Weather.current_condition.cloudcover
-	$Desc = $Weather.current_condition.weatherDesc.value
-	$Area = $Weather.nearest_area.areaName.value
-	$Region = $Weather.nearest_area.region.value
+	$Visib = $Weather.current_condition.visibility
 
-	& "$PSScriptRoot/_reply.ps1" "$($Temp)°C, $($Precip)mm rain, $($Humidity)% humidity, $($WindSpeed)km/h wind from $WindDir with $($Clouds)% clouds and $($Visib)km visibility at $Area ($Region)."
+	& "$PSScriptRoot/_reply.ps1" "$Temp degrees celcius, $($Precip)mm rain, $($Humidity)% humidity, $($WindSpeed)km/h wind from $WindDir with $($Clouds)% clouds and $($Visib)km visibility."
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
