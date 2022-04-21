18ba702e12b12fbea243f756bd0594890efe6fb8
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-21 15:05:23 +0200

    Rename to when-is-tea-time.ps1

--- scripts/when-is-tea-time.ps1
+++ scripts/when-is-tea-time.ps1
@@ -1,17 +1,17 @@
 <#
 .SYNOPSIS
-	Checks for Tea time
+	Replies the time until Tea time
 .DESCRIPTION
 	This PowerShell script checks the time until Tea time and replies by text-to-speech (TTS).
 .EXAMPLE
-	PS> ./check-tea-time
-.NOTES
-	Author: Markus Fleschutz / License: CC0
+	PS> ./when-is-tea-time
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-function TimeSpanToString { param([TimeSpan]$Delta)
+function TimeSpan2String { param([TimeSpan]$Delta)
 	$Result = ""
 	if ($Delta.Hours -eq 1) {       $Result += "1 hour and "
 	} elseif ($Delta.Hours -gt 1) { $Result += "$($Delta.Hours) hours and "
@@ -26,13 +26,13 @@ try {
 	$Now = [DateTime]::Now
 	$TeaTime = Get-Date -Hour 16 -Minute 0 -Second 0
 	if ($Now -lt $TeaTime) {
-		$TimeSpan = TimeSpanToString($TeaTime - $Now)
+		$TimeSpan = TimeSpan2String($TeaTime - $Now)
 		$Reply = "Tea time is in $TimeSpan."
 	} else {
-		$TimeSpan = TimeSpanToString($Now - $TeaTime)
+		$TimeSpan = TimeSpan2String($Now - $TeaTime)
 		$Reply = "Tea time was $TimeSpan ago."
 	}
-	& "$PSScriptRoot/_reply.ps1" "$Reply"
+	& "$PSScriptRoot/_reply.ps1" $Reply
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
