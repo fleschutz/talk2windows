6be4c4c3cd97c9f07543b5ff83566c1b3d306d24
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-25 09:03:04 +0200

    Update open-update-settings.ps1

--- scripts/open-update-settings.ps1
+++ scripts/open-update-settings.ps1
@@ -1,16 +1,16 @@
 <#
 .SYNOPSIS
-	Opens the update settings of Windows
+	Opens Windows Update
 .DESCRIPTION
 	This PowerShell script launches the update settings of Windows.
 .EXAMPLE
 	PS> ./open-update-settings
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-& "$PSScriptRoot/_reply.ps1" "Update..."
+& "$PSScriptRoot/_reply.ps1" "Just a moment."
 Start-Process ms-settings:windowsupdate
 exit 0 # success
