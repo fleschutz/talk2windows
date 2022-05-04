d1ffee42c17f7667c76a4b6d0c89f221976a2546
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-05-04 11:33:44 +0200

    Update open-downloads-folder.ps1

--- scripts/open-downloads-folder.ps1
+++ scripts/open-downloads-folder.ps1
@@ -5,18 +5,18 @@
 	This PowerShell script launches the File Explorer showing the user's downloads folder.
 .EXAMPLE
 	PS> ./open-downloads-folder
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 try {
-	$TargetDir = resolve-path "$HOME/Downloads"
-	if (-not(test-path "$TargetDir" -pathType container)) {
-		throw "Downloads folder at $TargetDir doesn't exist (yet)"
+	$Path = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+	if (-not(Test-Path "$Path" -pathType container)) {
+		throw "Downloads folder at $Path doesn't exist (yet)"
 	}
-	& "$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
+	& "$PSScriptRoot/open-file-explorer.ps1" $Path
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
