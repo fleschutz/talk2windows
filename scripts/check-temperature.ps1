5ed7acc988248792689c7b296f9d63320298e3b8
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-06-01 13:55:18 +0200

    Update check-temperature.ps1

--- scripts/check-temperature.ps1
+++ scripts/check-temperature.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .SYNOPSIS
-	Checks the Temperature
+	Checks the temperature
 .DESCRIPTION
-	Queries the current temperature and replies by text-to-speech (TTS).
+	This PowerShell script queries the current temperature and replies by text-to-speech (TTS).
 .EXAMPLE
 	PS> ./check-temperature
 .LINK
@@ -22,11 +22,15 @@ try {
 		if ($Temp -gt $MaxTemp) { $MaxTemp = $Temp; $MaxTime = $Hourly.time }
 		if ($Hourly.time -eq "2100") { break }
 	}
-	if ($CurTemp -eq $MaxTemp) {
-		$Reply = "$CurTemp °C outside."
-	} else {
-		if ($MaxTime -lt 1200) { $Time = "$($MaxTime / 100) AM" } else { $Time = "$(($MaxTime - 1200) / 100) PM" }
+	if ($CurTemp -lt $MaxTemp) {
+		if ($MaxTime -eq 1200) {       $Time = "12 PM"
+		} elseif ($MaxTime -eq 0) {    $Time = "12 AM"
+		} elseif ($MaxTime -lt 1200) { $Time = "$($MaxTime / 100) AM"
+		} else {                       $Time = "$(($MaxTime - 1200) / 100) PM"
+		}
 		$Reply = "$CurTemp °C outside, up to $MaxTemp °C expected at $Time."
+	} else {
+		$Reply = "$CurTemp °C outside."
 	}
 	& "$PSScriptRoot/_reply.ps1" $Reply
 	exit 0 # success
