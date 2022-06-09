568add041ed4096ca730854d96258795a014cc06
Markus Fleschutz <markus@fleschutz.de>
2022-06-09 16:20:25 +0200

    Update log-off.ps1 and minimize-all-windows.ps1

--- scripts/minimize-all-windows.ps1
+++ scripts/minimize-all-windows.ps1
@@ -5,12 +5,17 @@
 	This PowerShell script minimizes all open windows.
 .EXAMPLE
 	PS> ./minimize-all-windows
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-$shell = New-Object -ComObject "Shell.Application"
-$shell.minimizeall()
-exit 0 # success
+try {
+	$shell = New-Object -ComObject "Shell.Application"
+	$shell.minimizeall()
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
