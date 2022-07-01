8725faed592ddc014909f76a1d265d3ec551298a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-07-01 09:26:10 +0200

    Add check-gravity.ps1

---
+++ scripts/check-gravity.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Checks the gravity (fun)
+.DESCRIPTION
+	This PowerShell script checks the current gravity and replies by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./check-gravity
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/_reply.ps1" "Earth gravity is 9.8 meter per second squared."
+exit 0 # success
