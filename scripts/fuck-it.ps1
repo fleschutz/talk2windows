cb82481274e74c9061d53581f679b91c54a85b16
Markus Fleschutz <markus@fleschutz.de>
2022-08-29 22:17:57 +0200

    Add fuck-this.ps1 and fuck-it.ps1

---
+++ scripts/fuck-it.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Replies to "Fuck it"
+.DESCRIPTION
+	This PowerShell script replies to 'Fuck it' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./fuck-it
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/_reply.ps1" "Okay."
+exit 0 # success
