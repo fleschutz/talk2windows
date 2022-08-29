82b532c7ee21f550d6109f0dea5a6a47b7976d9f
Markus Fleschutz <markus@fleschutz.de>
2022-08-29 22:35:07 +0200

    Add call-the-police.ps1

---
+++ scripts/call-the-police.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	Replies to "Call the police"
+.DESCRIPTION
+	This PowerShell script replies to 'Call the police' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./call-the-police
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/_reply.ps1" "Will do."
+exit 0 # success
