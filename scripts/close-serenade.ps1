6623a865c23a0f27c05d8d88de665c88d078968e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-12-19 16:43:28 +0100

    Update close-serenade.ps1

--- scripts/close-serenade.ps1
+++ scripts/close-serenade.ps1
@@ -1,16 +1,16 @@
 <#
 .SYNOPSIS
-	Closes the Serenade.ai application
+	Closes Serenade
 .DESCRIPTION
-	This PowerShell script closes the Serenade.ai application gracefully.
+	This PowerShell script closes the Serenade application gracefully.
 .EXAMPLE
 	PS> ./close-serenade
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
-& "$PSScriptRoot/_reply.ps1" "Okay."
+& "$PSScriptRoot/_reply.ps1" "Bye bye."
 TaskKill /im Serenade.exe /f
 exit 0 # success
