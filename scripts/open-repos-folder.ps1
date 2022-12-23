9511127573b72f54df1184b46e44e12a9eae42d0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-12-23 07:52:00 +0100

    Update open-repos-folder.ps1 and open-repositories-folder.ps1

--- scripts/open-repos-folder.ps1
+++ scripts/open-repos-folder.ps1
@@ -12,16 +12,16 @@
 #>
 
 try {
-        if (Test-Path "$HOME/Repos" -pathType Container) {              # try short name
-		 $Path = "$HOME/Repos"
-	} elseif (Test-Path "$HOME/Repositories" -pathType Container) { # try long name
-                $Path = "$HOME/Repositories"
-	} elseif (Test-Path "$HOME/source/repos" -pathType Container) { # try Visual Studio default
-                $Path = "$HOME/source/repos"
+        if (Test-Path "$HOME\Repos" -pathType Container) {              # try short name
+		 $Path = "$HOME\Repos"
+	} elseif (Test-Path "$HOME\Repositories" -pathType Container) { # try long name
+                $Path = "$HOME\Repositories"
+	} elseif (Test-Path "$HOME\source\repos" -pathType Container) { # try Visual Studio default
+                $Path = "$HOME\source\repos"
         } else {
                 throw "The folder for Git repositories in your home directory doesn't exist (yet)."
         }
-	& "$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
+	& "$PSScriptRoot/open-file-explorer.ps1" "$Path"
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
