ca2fbf837acca4687c4550ff86ae17e41ed84e88
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-12-23 11:26:17 +0100

    Update when-is-christmas.ps1

--- scripts/when-is-christmas.ps1
+++ scripts/when-is-christmas.ps1
@@ -5,28 +5,28 @@
 	This PowerShell script replies to 'When is Christmas?' by text-to-speech (TTS).
 .EXAMPLE
 	PS> ./when-is-christmas
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 try {
 	$Now = [DateTime]::Now
-	$Christmas = [Datetime]("$($Now.Year)-12-25 23:59:59")
+	$Christmas = [Datetime]("$($Now.Year)-12-24 23:59:59")
 	$Days = ($Christmas - $Now).Days
 
 	if ($Days -gt 1) {
-		& "$PSScriptRoot/_reply.ps1" "Christmas is in $Days days."
+		& "$PSScriptRoot/_reply.ps1" "Christmas Eve is in $Days days."
 	} elseif ($Days -eq 1) {
-		& "$PSScriptRoot/_reply.ps1" "Christmas is tomorrow."
+		& "$PSScriptRoot/_reply.ps1" "Christmas Eve is tomorrow."
 	} elseif ($Days -eq 0) {
-		& "$PSScriptRoot/_reply.ps1" "Today is Christmas!"
+		& "$PSScriptRoot/_reply.ps1" "Today is Christmas Eve!"
 	} elseif ($Days -eq -1) {
-		& "$PSScriptRoot/_reply.ps1" "Christmas was yesterday."
+		& "$PSScriptRoot/_reply.ps1" "Christmas Eve was yesterday."
 	} else {
 		$Days = -$Days
-		& "$PSScriptRoot/_reply.ps1" "Christmas was $Days days ago."
+		& "$PSScriptRoot/_reply.ps1" "Christmas Eve was $Days days ago."
 	}
 	exit 0 # success
 } catch {
