4c1d34ba8528c5688c5d15cd8f0fd262d22eb04c
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-12-23 10:28:03 +0100

    Add open-XYZ-repo.ps1 and open-XYZ-repository.ps1

---
+++ scripts/open-XYZ-repository.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+	Opens a repository
+.DESCRIPTION
+	This PowerShell script launches the File Explorer with the given repository.
+.PARAMETER FolderName
+	Specifies the repository's folder name
+.EXAMPLE
+	PS> ./open-XYZ-repository
+.LINK
+	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$FolderName)
+
+try {
+        if (Test-Path "$HOME\Repos" -pathType Container) {              # try short name
+		 $Path = "$HOME\Repos"
+	} elseif (Test-Path "$HOME\Repositories" -pathType Container) { # try long name
+                $Path = "$HOME\Repositories"
+	} elseif (Test-Path "$HOME\source\repos" -pathType Container) { # try Visual Studio default
+                $Path = "$HOME\source\repos"
+        } else {
+                throw "The folder for Git repositories in your home directory doesn't exist (yet)."
+        }
+	if (-not(Test-Path "$Path\$FolderName" -pathType Container)) { throw "There's no repository called '$FolderName'" }
+	& "$PSScriptRoot/open-file-explorer.ps1" "$Path\$FolderName"
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
