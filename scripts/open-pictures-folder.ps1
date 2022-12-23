b8b40f1e557aa62a3001219f4d95fffcfaebbdff
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-12-23 10:50:04 +0100

    Update open-pictures-folder.ps1

--- scripts/open-pictures-folder.ps1
+++ scripts/open-pictures-folder.ps1
@@ -5,18 +5,18 @@
 	This PowerShell script launches the File Explorer with the user's pictures folder.
 .EXAMPLE
 	PS> ./open-pictures-folder
-.NOTES
-	Author: Markus Fleschutz / License: CC0
 .LINK
 	https://github.com/fleschutz/talk2windows
+.NOTES
+	Author: Markus Fleschutz | License: CC0
 #>
 
 try {
-	$TargetDir = resolve-path "$HOME/Pictures"
-	if (-not(test-path "$TargetDir" -pathType container)) {
-		throw "Pictures folder at $TargetDir doesn't exist (yet)"
+	$Path = [Environment]::GetFolderPath('MyPictures')
+	if (-not(Test-Path "$Path" -pathType container)) {
+		throw "Pictures folder at $Path doesn't exist (yet)"
 	}
-	& "$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
+	& "$PSScriptRoot/open-file-explorer.ps1" "$Path"
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
