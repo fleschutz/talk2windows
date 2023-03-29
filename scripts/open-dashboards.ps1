eeb40acf8e2c7c1a439198ef8bf2330f50bbe1eb
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-03-29 11:32:30 +0200

    Update open-dashboards.ps1

--- scripts/open-dashboards.ps1
+++ scripts/open-dashboards.ps1
@@ -12,16 +12,17 @@
 #>
 
 try {
-	& "$PSScriptRoot/_reply.ps1" "Okay."
-	& "$PSScriptRoot/open-browser.ps1" https://track.toggl.com/timer
-	& "$PSScriptRoot/open-browser.ps1" https://news.google.com
-	& "$PSScriptRoot/open-browser.ps1" https://www.windy.com/de/-Wetterradar-radar
-	& "$PSScriptRoot/open-browser.ps1" https://www.arcgis.com/apps/dashboards/c8af9c5411814584b460cc87cb7c3780
-	& "$PSScriptRoot/open-browser.ps1" https://radio.garden/visit/kempten/V5Ryr1S4
-	& "$PSScriptRoot/open-browser.ps1" https://www.foto-webcam.eu/
-	& "$PSScriptRoot/open-browser.ps1" https://sunnyportal.com/FixedPages/Dashboard.aspx
-	& "$PSScriptRoot/open-browser.ps1" https://covid19.who.int/
-	& "$PSScriptRoot/open-browser.ps1" https://top10.netflix.com/
+	& "$PSScriptRoot/_reply.ps1" "Hold on."
+	& "$PSScriptRoot/open-browser.ps1" "https://track.toggl.com/timer"
+	& "$PSScriptRoot/open-browser.ps1" "https://news.google.com"
+	& "$PSScriptRoot/open-browser.ps1" "https://www.windy.com/de/-Wetterradar-radar"
+	& "$PSScriptRoot/open-browser.ps1" "https://www.flightradar24.com/27.63,-6.98/3"
+	& "$PSScriptRoot/open-browser.ps1" "https://www.arcgis.com/apps/dashboards/c8af9c5411814584b460cc87cb7c3780"
+	& "$PSScriptRoot/open-browser.ps1" "https://radio.garden"
+	& "$PSScriptRoot/open-browser.ps1" "https://www.foto-webcam.eu"
+	& "$PSScriptRoot/open-browser.ps1" "https://sunnyportal.com/FixedPages/Dashboard.aspx"
+	& "$PSScriptRoot/open-browser.ps1" "https://covid19.who.int"
+	& "$PSScriptRoot/open-browser.ps1" "https://top10.netflix.com"
 	exit 0 # success
 } catch {
 	 & "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
