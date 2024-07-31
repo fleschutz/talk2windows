f0c94f4ae6c61f63c8e6dd6ecf3f42f941053225
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-07-31 13:20:45 +0200

    Updated some scripts

--- scripts/see-you.ps1
+++ scripts/see-you.ps1
@@ -5,7 +5,7 @@
 	This PowerShell script says a reply to "see you" by text-to-speech (TTS).
 #>
 
-$reply = "Bye!", "Bye bye!", "Good bye!", "See you!", "See ya!", "Cheers!", "So long!", "Take care!" | Get-Random
+$reply = "Bye!", "Bye bye!", "Good bye!", "See you!", "Cheers!", "So long!", "Take care!" | Get-Random
 
 & "$PSScriptRoot/_reply.ps1" $reply
 exit 0 # success
