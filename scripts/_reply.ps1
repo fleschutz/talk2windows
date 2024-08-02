42ec631d9c205058df995096ed17136af2e46e84
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-02 10:33:44 +0200

    Updated thank-you.ps1 and _reply.ps1

--- scripts/_reply.ps1
+++ scripts/_reply.ps1
@@ -1,19 +1,11 @@
-<#
-.SYNOPSIS
-	Gives a reply 
-.DESCRIPTION
-	This PowerShell script replies by text-to-speech (TTS) in English.
-#>
+param([string]$txt="")
+$TTS=New-Object -ComObject SAPI.SPVoice
+foreach($v in $TTS.GetVoices()) {if($v.GetDescription() -like "*- English*") {$TTS.Voice=$v}}
+[void]$TTS.Speak($txt)
 
-param([string]$text = "")
-
-$TTS = New-Object -ComObject SAPI.SPVoice
-foreach($v in $TTS.GetVoices()) { if ($v.GetDescription() -like "*- English*") { $TTS.Voice = $v } }
-[void]$TTS.Speak($text)
-
-if ("$env:TEMP" -ne "")	{	$TempDir = "$env:TEMP"
-} elseif ("$env:TMP" -ne "") {	$TempDir = "$env:TMP"
-} elseif ($IsLinux) {		$TempDir = "/tmp"
-} else {			$TempDir = "C:\Temp" }
+if("$env:TEMP" -ne "")	{	$TempDir="$env:TEMP"
+} elseif("$env:TMP" -ne "") {	$TempDir="$env:TMP"
+} elseif($IsLinux) {		$TempDir="/tmp"
+} else{				$TempDir="C:\Temp" }
 "$text" > "$TempDir/talk2windows_last_reply.txt"
 exit 0
--- scripts/thank-you.ps1
+++ scripts/thank-you.ps1
@@ -3,14 +3,8 @@
 	Replies to 'thank you'
 .DESCRIPTION
 	This PowerShell script replies to 'thank you' by text-to-speech (TTS).
-.EXAMPLE
-	PS> ./thank-you
-.LINK
-	https://github.com/fleschutz/PowerShell
-.NOTES
-	Author: Markus Fleschutz | License: CC0
 #>
 
-$Reply = "You're welcome.", "No worries.", "Don't mention it.", "My pleasure.", "Pleasure is mine.", "Glad to help.", "Anytime." | Get-Random
-& "$PSScriptRoot/_reply.ps1" $Reply
+$reply = "You're welcome.", "No worries.", "Don't mention it.", "My pleasure.", "Pleasure is mine.", "Glad to help.", "Anytime." | Get-Random
+& "$PSScriptRoot/_reply.ps1" $reply
 exit 0 # success
