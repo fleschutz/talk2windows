d5dcadf62996055eeba39c49f6dd8fba70b92c2f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-07 10:01:34 +0200

    Renamed to open-attower-manuals.ps1

--- scripts/open-attower-manuals.ps1
+++ scripts/open-attower-manuals.ps1
@@ -1,14 +1,8 @@
 <#
 .SYNOPSIS
-	Shows the ATTower manual
+	Opens the ATTower manual
 .DESCRIPTION
 	This PowerShell script launches the Web browser with the ATTower manual.
-.EXAMPLE
-	PS> ./show-tower-manual
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
 & "$PSScriptRoot/open-browser.ps1" "https://ufa365-my.sharepoint.com/personal/markus_fleschutz_atcsim_de/_layouts/15/onedrive.aspx?id=%2Fsites%2FProductManuals%2FShared%20Documents%2FATTower&listurl=https%3A%2F%2Fufa365%2Esharepoint%2Ecom%2Fsites%2FProductManuals%2FShared%20Documents&viewid=52ccd99a%2D45b8%2D4a68%2D852d%2D0ba5ac10ef63"
