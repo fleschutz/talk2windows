14c1770d088b3e705d825dd6f5300fecffe35e7a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-07 16:46:36 +0200

    Updated open-google-calendar.ps1

--- scripts/open-google-calendar.ps1
+++ scripts/open-google-calendar.ps1
@@ -3,14 +3,8 @@
 	Opens Google Calendar
 .DESCRIPTION
 	This PowerShell script launches the Web browser with Google Calendar.
-.EXAMPLE
-	PS> ./open-google-calendar
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
-& "$PSScriptRoot/_reply.ps1" "Calendar..."
 & "$PSScriptRoot/open-browser.ps1" "https://calendar.google.com"
+& "$PSScriptRoot/_reply.ps1" "Okay."
 exit 0 # success
