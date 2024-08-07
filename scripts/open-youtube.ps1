5ce5a81b16d8d88a26a57a7bde9794e1d5f86f4f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-07 09:26:20 +0200

    Updated open-youtube.ps1

--- scripts/open-youtube.ps1
+++ scripts/open-youtube.ps1
@@ -3,14 +3,8 @@
 	Opens YouTube 
 .DESCRIPTION
 	This PowerShell script launches the Web browser with YouTube.com
-.EXAMPLE
-	PS> ./open-youtube
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
 & "$PSScriptRoot/_reply.ps1" "Okay."
-& "$PSScriptRoot/open-browser.ps1" "https://www.youtube.com/results?search_query=Summer+Mega+Hits+2023"
+& "$PSScriptRoot/open-browser.ps1" "https://www.youtube.com/results?search_query=Mega+Hits+2024"
 exit 0 # success
