9dad85d3564742e40f70971e42aeaa74fe0c7fc0
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-08-13 15:22:01 +0200

    Updated some scripts

---
+++ scripts/open-XYZ-dot-com.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+	Opens XYZ.com
+.DESCRIPTION
+	This PowerShell script launches the Web browser with the given .com website.
+#>
+
+param([string]$name = "")
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.$($name).com"
+exit 0 # success
--- scripts/open-windy-website.ps1
+++ scripts/open-windy-website.ps1
@@ -3,13 +3,7 @@
 	Opens the windy.com website
 .DESCRIPTION
 	This PowerShell script launches the Web browser with the windy.com website.
-.EXAMPLE
-	PS> ./open-windy-website
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
-& "$PSScriptRoot/open-browser.ps1" "https://www.windy.com"
+& "$PSScriptRoot/open-browser.ps1" "https://www.windy.com/-Weather-radar-radar"
 exit 0 # success
