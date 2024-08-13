400c0bdcecc261c88e4c2610bdfd5992370ff738
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-08-13 11:47:59 +0200

    Added some scripts

---
+++ scripts/are-you-drunk.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+	Replies to: 'Are you drunk?'
+.DESCRIPTION
+	This PowerShell script replies to 'Are you drunk?' by text-to-speech (TTS).
+#>
+
+$reply = "Not now.", "Sometimes." | Get-Random
+& "$PSScriptRoot/_reply.ps1" $reply
+exit 0
