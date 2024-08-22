ec075dbdde9d87bc218cff63d298bb64dd5278c2
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-22 13:37:56 +0200

    Updated open-home-folder.ps1

--- scripts/open-home-folder.ps1
+++ scripts/open-home-folder.ps1
@@ -3,20 +3,13 @@
 	Opens the home folder
 .DESCRIPTION
 	This PowerShell script launches the File Explorer with the user's home folder.
-.EXAMPLE
-	PS> ./open-home-folder
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
 try {
-	$TargetDir = resolve-path "$HOME"
-	if (-not(test-path "$TargetDir" -pathType container)) {
-		throw "Home folder at $TargetDir doesn't exist (yet)"
-	}
-	& "$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
+	$path = Resolve-Path "~"
+	if (-not(Test-Path "$path" -pathType container)) { throw "Home folder at $path doesn't exist (yet)" }
+	& "$PSScriptRoot/open-file-explorer.ps1" "$path"
+	& "$PSScriptRoot/_reply.ps1" "Okay."
 	exit 0 # success
 } catch {
 	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
