583ddc34d5eef6e9a49b95105f61283c32ec8eea
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-27 14:26:11 +0200

    Added open-control-panel.ps1

---
+++ scripts/open-control-panel.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+	Launch Control Panel
+.DESCRIPTION
+	This PowerShell script launches the Windows Control Panel.
+#>
+
+try {
+	Start-Process control.exe
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
