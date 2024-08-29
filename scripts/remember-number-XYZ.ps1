7dd29e232e5c622dd8a5a80f679932c2a559c1fe
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-29 16:04:25 +0200

    Added remember-number-XYZ.ps1 and what-was-the-number.ps1

---
+++ scripts/remember-number-XYZ.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+	Remember number <N>
+.DESCRIPTION
+	This PowerShell script remembers the given number for later use.
+#>
+
+param([int]$number)
+
+function GetTempDir {
+        if ("$env:TEMP" -ne "") { return "$env:TEMP" }
+        if ("$env:TMP" -ne "")  { return "$env:TMP" }
+        return "C:\Temp"
+}
+
+try {
+	$path = GetTempDir
+	if (-not(Test-Path "$path" -pathType container)) { throw "The temporary folder at $path doesn't exist yet." }
+	"$number" > "$path\talk2windows_number.txt"
+	$reply = "OK, $number remembered."
+} catch { $reply = "Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
---
+++ scripts/what-was-the-number.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+	Tells the remembered number.
+.DESCRIPTION
+	This PowerShell script tells the remembered number.
+#>
+
+function GetTempDir {
+        if ("$env:TEMP" -ne "") { return "$env:TEMP" }
+        if ("$env:TMP" -ne "")  { return "$env:TMP" }
+        return "C:\Temp"
+}
+
+try {
+	$path = GetTempDir
+	if (-not(Test-Path "$path" -pathType container)) { throw "The temporary folder at $path doesn't exist yet." }
+	$path = "$path\talk2windows_number.txt"
+	if (-not(Test-Path "$path" -pathType leaf)) { throw "I forgot." }
+	$reply = "The number was " + (Get-Content "$path")
+} catch { $reply = "Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
