811d27724ac29cc848e07c83d676a990c823f607
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-08-30 11:27:15 +0200

    Added insert-number-XYZ.ps1

---
+++ scripts/insert-number-XYZ.ps1
@@ -0,0 +1,10 @@
+﻿<#
+.SYNOPSIS
+	Insert number <X>
+.DESCRIPTION
+	This PowerShell script inserts the given number at the current text cursor position.
+#>
+
+param([int]$number)
+(New-Object -com wscript.shell).SendKeys("$number")
+& "$PSScriptRoot/_reply.ps1" "$number inserted."
