d4e45bcc3e49eb7744384352e4f6f025936d75da
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-01 14:26:18 +0200

    Updated close-serenade.ps1

--- scripts/close-serenade.ps1
+++ scripts/close-serenade.ps1
@@ -5,6 +5,6 @@
 	This PowerShell script closes the Serenade application gracefully.
 #>
 
-& "$PSScriptRoot/_reply.ps1" "Okay, see you later."
+& "$PSScriptRoot/_reply.ps1" "Okay, see you."
 TaskKill /im Serenade.exe /f
 
