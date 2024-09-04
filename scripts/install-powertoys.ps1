8178ee121ab3229c1be0c5bb134f2d12da30f432
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-04 10:44:33 +0200

    Added install-powertoys.ps1

---
+++ scripts/install-powertoys.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+	Installs Powertoys
+.DESCRIPTION
+	This PowerShell script installs Powertoys from the Microsoft Store.
+#>
+
+try {
+	& "$PSScriptRoot/_reply.ps1" "Installing Powertoys, please wait..."
+
+	& winget install --id XP89DCGQ3K6VLD --accept-package-agreements --accept-source-agreements
+	if ($lastExitCode -ne "0") { throw "Installation of Powertoys failed, maybe it's already installed" }
+
+	$reply = "Powertoys installed successfully."
+} catch { $reply = Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
