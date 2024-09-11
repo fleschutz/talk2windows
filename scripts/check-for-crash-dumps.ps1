7f80b4948a6e18b62af7d0f65243867ebbf91ca5
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-11 14:39:57 +0200

    Added check-for-crash-dumps.ps1

---
+++ scripts/check-for-crash-dumps.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Check for crash dumps
+.DESCRIPTION
+	This PowerShell script checks if there are crash dump files.
+#>
+
+function CountCrashDumps {
+        [string]$path = Resolve-Path -Path "~\AppData\Local\CrashDumps"
+        $files = (Get-ChildItem -path "$path\*.dmp" -attributes !Directory)
+        return $files.Count
+}
+
+try {
+	[int]$count = CountCrashDumps
+	if ($count -eq 0) {
+		$reply = "No crash dump found."		
+	} elseif ($count -eq 1) {
+		$reply = "There's one crash dump."
+	} else {
+		$reply = "Oh, there are $count crash dumps."
+	}
+} catch { $reply = "Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
