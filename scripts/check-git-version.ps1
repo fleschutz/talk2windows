75e0f16ba20cb43705ea3fb6ff0ad502e8276263
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-18 08:03:39 +0200

    Updated check-git-version.ps1

--- scripts/check-git-version.ps1
+++ scripts/check-git-version.ps1
@@ -3,19 +3,10 @@
 	Checks the Git version
 .DESCRIPTION
 	This PowerShell script checks the Git version and answers it by text-to-speech (TTS).
-.EXAMPLE
-	PS> ./check-git-version
-.NOTES
-	Author: Markus Fleschutz / License: CC0
-.LINK
-	https://github.com/fleschutz/talk2windows
 #>
 
 try {
-	$Version = (git --version)
-	& "$PSScriptRoot/_reply.ps1" "It's $Version"
-	exit 0 # success
-} catch {
-	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
-	exit 1
-}
+	$version = (git --version)
+	$reply = "Installed is $Version."
+} catch { $reply = "Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
