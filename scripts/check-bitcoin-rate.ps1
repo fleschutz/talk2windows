0becb0327eac028b128efcc4a772f82c908f0632
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-19 11:12:23 +0200

    Added open-temp-folder.ps1

--- scripts/check-bitcoin-rate.ps1
+++ scripts/check-bitcoin-rate.ps1
@@ -9,9 +9,6 @@ try {
 	$rates = (Invoke-WebRequest -uri "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	$USD = [math]::round($rates.USD)
 	$EUR = [math]::round($rates.EUR)
-	& "$PSScriptRoot/_reply.ps1" "Bitcoin is at $USD US Dollar or $EUR Euro."
-	exit 0 # success
-} catch {
-	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
-	exit 1
-}
+	$reply = "Bitcoin is at $USD US Dollar or at $EUR Euro."
+} catch { $reply = "Sorry: $($Error[0])" }
+& "$PSScriptRoot/_reply.ps1" $reply
---
+++ scripts/open-temp-folder.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+	Opens the temporary folder
+.DESCRIPTION
+	This PowerShell script launches the File Explorer showing the temporary folder.
+#>
+
+function GetTempDir {
+	if ("$env:TEMP" -ne "")	{ return "$env:TEMP" }
+	if ("$env:TMP" -ne "")	{ return "$env:TMP" }
+	return "C:\Temp"
+}
+
+try {
+	$path = GetTempDir
+	if (-not(Test-Path "$path" -pathType container)) { throw "The temporary folder at $path doesn't exist yet." }
+	& "$PSScriptRoot/open-file-explorer.ps1" "$path"
+	& "$PSScriptRoot/_reply.ps1" "Here it is."
+	exit 0 # success
+} catch {
+	& "$PSScriptRoot/_reply.ps1" "Sorry: $($Error[0])"
+	exit 1
+}
