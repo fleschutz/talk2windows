5c3499debe9c92df5fb2d4caa72f8a7211b088d9
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-10-09 07:38:26 +0200

    Updated open-visual-studio.ps1

--- scripts/open-visual-studio.ps1
+++ scripts/open-visual-studio.ps1
@@ -5,17 +5,21 @@
 	This PowerShell script launches the Microsoft Visual Studio application.
 #>
 
-function TryLaunching { param($Path)
-	if (test-path "$Path" -pathType leaf) {
-		start-process "$Path"
-		exit 0 # success
-	}
+function TryToLaunch { param($path)
+	if (-not(Test-Path "$path" -pathType leaf)) { return }
+	Start-Process "$Path"
+	exit 0 # success
 }
 
 try {
-	TryLaunching "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"
-	TryLaunching "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
-	exit 0 # success
+	& "$PSScriptRoot/say.ps1" "Hold on."
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+	TryToLaunch "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
+	throw "It seems Visual Studio is not installed."
 } catch {
 	& "$PSScriptRoot/say.ps1" "Sorry: $($Error[0])"
 	exit 1
