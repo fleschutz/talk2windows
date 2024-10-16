dda217dca890324fb3cbfae83c6d60c82c86303b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-10-16 09:10:21 +0200

    Updated check-my-downloads.ps1

--- scripts/check-my-downloads.ps1
+++ scripts/check-my-downloads.ps1
@@ -1,27 +1,29 @@
 ﻿<#
 .SYNOPSIS
-	Check for downloads
+	Check my downloads
 .DESCRIPTION
 	This PowerShell script checks if there are downloads available.
 #>
 
 try {
 	[string]$path = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+
 	[int]$total = (Get-ChildItem -path "$path\*" -attributes !Directory).Count
+
 	if ($total -eq 0) {
 		$reply = "No downloads yet."
 	} else {
 		[int]$inProgress = (Get-ChildItem -path "$path\*.crdownload" -attributes !Directory).Count
-		if ($inProgress -eq 1) {	
-			$reply = "One download in progress, "
-		} elseif ($inProgress -gt 1) {	
-			$reply = "$inProgress downloads in progress, "
-		}
 		$total -= $inProgress
 		if ($total -eq 1) {
-			$reply += "One file downloaded."
+			$reply = "One file downloaded."
 		} else {
-			$reply += "$total files downloaded."
+			$reply = "$total files downloaded."
+		}
+		if ($inProgress -eq 1) {	
+			$reply += " One in progress."
+		} elseif ($inProgress -gt 1) {	
+			$reply += "$inProgress in progress."
 		}
 	}
 } catch { $reply = "Sorry: $($Error[0])" }
