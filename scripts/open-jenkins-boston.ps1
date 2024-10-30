a4a4362c4d2ef100005382e398902d769e7525b6
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-10-30 14:21:54 +0100

    Updated open-jenkins-one.ps1 and open-jenkins-boston.ps1

--- scripts/open-jenkins-boston.ps1
+++ scripts/open-jenkins-boston.ps1
@@ -5,4 +5,5 @@
 	This PowerShell script launches the Web browser with Jenkins Boston.
 #>
 
+& "$PSScriptRoot/say.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "http://rocket:8080"
--- scripts/open-jenkins-one.ps1
+++ scripts/open-jenkins-one.ps1
@@ -5,4 +5,5 @@
 	This PowerShell script launches the Web browser with Jenkins Kaufbeuren.
 #>
 
+& "$PSScriptRoot/say.ps1" "Okay."
 & "$PSScriptRoot/open-browser.ps1" "http://jenkins.ad.atcsim.de/jenkins/"
