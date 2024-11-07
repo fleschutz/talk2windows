b4532cf13ca0c9c1541c6a2c3bf82041c2f9e207
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-11-07 08:18:57 +0100

    Updated open-tower-XYZ.ps1

--- scripts/open-tower-XYZ.ps1
+++ scripts/open-tower-XYZ.ps1
@@ -5,14 +5,7 @@
 	This PowerShell script calculates the given expression and speaks the result by text-to-speech (TTS).
 #>
 
-param([string]$part1 = "", [string]$part2 = "", [string]$part3 = "", [string]$part4 = "", [string]$part5 = "", [string]$part6 = "", [string]$part7 = "")
+param([string]$JiraNumber = "")
 
-try {
-	$JiraNumber = "$part1 $part2 $part3 $part4 $part5 $part6 $part7"
-
-	& "$PSScriptRoot/open-browser.ps1" "https://ufainc.atlassian.net/browse/TOWER-$JiraNumber"
-	exit 0 # success
-} catch {
-	& "$PSScriptRoot/say.ps1" "Sorry: $($Error[0])"
-	exit 1
-}
+& "$PSScriptRoot/say.ps1" "Okay."
+& "$PSScriptRoot/open-browser.ps1" "https://ufainc.atlassian.net/browse/TOWER-$JiraNumber"
