d56eea49352385f2d9bebbbfd9fd2168349278dc
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-11-27 12:58:02 +0100

    Added open-flight-radar.ps1

---
+++ scripts/open-flight-radar.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+	Opens FlightRadar24
+.DESCRIPTION
+	This PowerShell script launches the Web browser with the FlightRadar24 website.
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.flightradar24.com"
