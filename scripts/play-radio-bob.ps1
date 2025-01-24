e84f2861538795cea0757133bb27024d0374efa8
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-01-24 11:20:22 +0100

    Updated play-radio-bob.ps1 and play-rock-music.ps1

--- scripts/play-radio-bob.ps1
+++ scripts/play-radio-bob.ps1
@@ -6,4 +6,5 @@
 #>
 
 & "$PSScriptRoot/say.ps1" "Oh yeah!"
-& "$PSScriptRoot/open-browser.ps1" "http://streema.com/radios/play/RADIO_BOB_BOBs_Alternative"
+& "$PSScriptRoot/open-browser.ps1" "https://tunein.com/radio/RADIO-BOB-Livestream-994-s96189/?lang=en"
+exit 0 # success
