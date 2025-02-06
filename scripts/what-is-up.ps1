ce8c0306d96cf4a0605d7ab3e2ceecee1e23fa82
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-02-06 09:38:28 +0100

    Updated what-is-up.ps1

--- scripts/what-is-up.ps1
+++ scripts/what-is-up.ps1
@@ -5,5 +5,5 @@
 	This PowerShell script replies to 'What's up?' by text-to-speech (TTS).
 #>
 
-$reply = "Hey! How's it going?", "What's happening?", "How goes it?", "I'm fine, how are you?", "Can't complain.", "Not much. How about you?", "Looking forward to the weekend.", "Just busy, busy! What about you?" | Get-Random
+$reply = "The usual. What’s up with you?", "Hey! How's it going?", "What's happening?", "How goes it?", "I'm fine, how are you?", "Can't complain.", "Not much. How about you?", "Ready for the weekend. And you?", "A bit stressed. And you?" | Get-Random
 & "$PSScriptRoot/say.ps1" $reply
