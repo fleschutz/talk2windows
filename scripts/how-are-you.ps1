0276f8178f59a7366d9f1f2e3e61e27704fe8306
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-02-06 09:31:50 +0100

    Updated how-are-you.ps1

--- scripts/how-are-you.ps1
+++ scripts/how-are-you.ps1
@@ -5,5 +5,5 @@
 	This PowerShell script replies to "How are you?" by text-to-speech (TTS).
 #>
 
-$reply = "I'm fine. How are you?", "Great, thank you. How are you?", "Good, thanks, and you?", "Fine, thanks. How are you?" | Get-Random
+$reply = "Fine. And you?", "I'm good. And you?", "I'm all right. Thanks.", "Fine, how are you?", "Pretty good. And you?", "Good, thanks, and you?", "Fine, thanks. How are you?" | Get-Random
 & "$PSScriptRoot/say.ps1" $reply
