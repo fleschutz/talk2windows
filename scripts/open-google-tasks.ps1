4ff9aceb2f9e2d91cc24de963cd25247d40d1b4b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-02-20 07:11:08 +0100

    Updated open-google-tasks.ps1

--- scripts/open-google-tasks.ps1
+++ scripts/open-google-tasks.ps1
@@ -5,5 +5,6 @@
 	This PowerShell script launches the Web browser with Google Tasks.
 #>
 
-& "$PSScriptRoot/say.ps1" "Hold on..."
-& "$PSScriptRoot/open-browser.ps1" "https://tasks.google.com/embed/?origin=https://calendar.google.com&fullWidth=1"
+& "$PSScriptRoot/open-browser.ps1" "https://tasks.google.com/tasks/"
+& "$PSScriptRoot/say.ps1" "Your tasks."
+
