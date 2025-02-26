79ca3f701cbc105018cd148cd84e6e9240a6b3ff
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-02-26 08:42:11 +0100

    Improved open-jenkins-XYZ.ps1

--- scripts/open-jenkins-XYZ.ps1
+++ scripts/open-jenkins-XYZ.ps1
@@ -8,12 +8,19 @@
 param([string]$name)
 
 switch($name) {
+"Blog"		{ $URL = "https://www.jenkins.io/blog/" }
 "Boston"	{ $URL = "http://rocket:8080" }
+"Docs"		{ $URL = "https://www.jenkins.io/doc/book/" }
+"Documentation" { $URL = "https://www.jenkins.io/doc/book/" }
+"Downloads"     { $URL = "https://www.jenkins.io/download/" }
 "Homepage"	{ $URL = "https://www.jenkins.io" }
 "Germany"	{ $URL = "http://jenkins.ad.atcsim.de/jenkins/" }
+"Glossary"	{ $URL = "https://www.jenkins.io/doc/book/glossary/" }
+"Handbook"      { $URL = "https://www.jenkins.io/doc/book/" }
 "Kaufbeuren"	{ $URL = "http://jenkins.ad.atcsim.de/jenkins/" }
 "Manual"	{ $URL = "https://www.jenkins.io/doc/book/" }
 "Plugins"	{ $URL = "https://plugins.jenkins.io/" }
+"Tutorials"     { $URL = "https://www.jenkins.io/doc/tutorials/" }
 "Website"	{ $URL = "https://www.jenkins.io" }
 "USA"		{ $URL = "http://rocket:8080" }
 default		{ & "$PSScriptRoot/say.ps1" "Sorry, say again?"; exit 1 }
