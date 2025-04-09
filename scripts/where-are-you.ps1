5524021cc1e990b7d53f2cb5e17c7b4826e4338e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-04-09 10:27:30 +0200

    Updated where-are-you.ps1

--- scripts/where-are-you.ps1
+++ scripts/where-are-you.ps1
@@ -5,5 +5,6 @@
 	This PowerShell script replies to 'Where are you?' by text-to-speech (TTS).
 #>
 
-$reply = "I'm here.","Here.","At your side.","Next to you.","In the computer.","In a CPU." | Get-Random
+$reply = "I'm here.","Here.","At your side.","Next to you.","In your computer." | Get-Random
 & "$PSScriptRoot/say.ps1" $reply
+exit 0 # success
