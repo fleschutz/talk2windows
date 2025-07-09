fd44a1f91a32a3ab9d62450b312a3acba93f0620
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-07-09 15:49:45 +0200

    Added open-iso-9001.ps1

---
+++ scripts/open-iso-9001.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+	Opens ISO9001
+.DESCRIPTION
+	This PowerShell script launches the Web browser with ISO9001 website.
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://ufa365.sharepoint.com/sites/QMS/Shared%20Documents/Forms/AllItems.aspx?csf=1&web=1&e=NhKepu&CID=89e3ed7f%2D5231%2D74f3%2Dcfcd%2D11970ca22b96&FolderCTID=0x01200049288238B647E84CB96C1463E88BA623&id=%2Fsites%2FQMS%2FShared%20Documents%2FISO9001%20%E2%80%93%20Staff%20Distribution"
+& "$PSScriptRoot/say.ps1" "Here's it."
